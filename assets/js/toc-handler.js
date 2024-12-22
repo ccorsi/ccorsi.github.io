0a1f7824c4c8e5fb5e250873ee712e6065c94e14
Claudio Corsi <clcorsi@yahoo.com>
2024-12-21 21:40:23 -0500

    Updated the table of content script to include the 'Table of Content' title and a line seperating it from the text

--- assets/js/toc-handler.js
+++ assets/js/toc-handler.js
@@ -7,23 +7,36 @@ $( document ).ready(function() {
    console.debug('TOC: Inside Table of Content ready callback');
   // Handler for .ready() called.
 
-  $('#toc').toc({ 
-    minimumHeaders: 0,
-    listType: 'ul',
-    showSpeed: 0,
-    headers: 'h2,h3,h4'
-  });
+  try {
+     $('#toc').toc({
+       minimumHeaders: 0,
+       listType: 'ul',
+       showSpeed: 0,
+       headers: 'h2,h3,h4'
+     });
 
-  /* this offset helps account for the space taken up by the floating toolbar. */
-  $('#toc').on('click', 'a', function() {
-    var target = $(this.getAttribute('href')),
-      scroll_target = target.offset().top
+     /* determine if there are any enteries that will be used to create a Table of Content */
+     if ( $( '#toc' ).children().first().length > 0 ) {
+        /* Add a Table of Content Heading */
+        $( '#toc' ).children().first().prepend( $( "<h2>" ).append( $( "<u>" ).text('Table of Content') ).css("text-align", "center") )
+        $( '#toc' ).children().first().append( $( "<hr>" ) )
+        console.debug('TOC: Added a Table Of Content')
+     }
 
-    $(window).scrollTop(scroll_target - 10);
+     /* this offset helps account for the space taken up by the floating toolbar. */
+     $('#toc').on('click', 'a', function() {
+       var target = $(this.getAttribute('href')),
+         scroll_target = target.offset().top
 
-    return false
-  })
+       $(window).scrollTop(scroll_target - 10);
 
+       return false
+     })
+  } catch (exc) {
+     console.debug(`TOC: An exception was raised while processing Table of Context: ${exc}`)
+  } finally {
+     console.debug('TOC: Exiting Table of Content ready callback');
+  }
 });
 // Copyright 2021 Google LLC
 // Copyright 2024 Claudio Corsi
