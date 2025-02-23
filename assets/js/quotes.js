d88af7ce0cbec10ccfed7c3438a3714d73fc9a5c
Claudio Corsi <clcorsi@yahoo.com>
2025-02-23 15:30:57 -0500

    Updated the quotes functionality to be able to better handle html code within the quotes entries themselves.
    Moved the Mathish book from reading to read table.

--- assets/js/quotes.js
+++ assets/js/quotes.js
@@ -7,74 +7,80 @@
 	// given url for the passed element id.
 	function getQuote(url, elm) {
 
-	  console.debug('GetQuote: Inside getQuote call');
-	  let request = new XMLHttpRequest();
-	  request.open('GET', url);
-	  request.responseType = 'json';
-	  request.send();
-	  request.onload = () => {
+		console.debug('GetQuote: Inside getQuote call');
+		let request = new XMLHttpRequest();
+		request.open('GET', url);
+		request.responseType = 'json';
+		request.send();
+		request.onload = () => {
 
-	     console.debug('GetQuote: the request has completed successfully.');
+			console.debug('GetQuote: the request has completed successfully.');
 
-	     // This method will randomly select a quote and then display it
-	     const quotes = request.response;
-	     const quote = quotes[getRandomInt(quotes.length)];
-	     const formatted = quote["formatted"];
-	     var div = document.createElement('div');
+			// This method will randomly select a quote and then display it
+			const quotes = request.response;
+			const quote = quotes[getRandomInt(quotes.length)];
+			const formatted = quote["formatted"];
+			var div = document.createElement('div');
 
-	     div.setAttribute('style','text-align: left;');
-	     div.append(document.createElement('br'));
+			div.setAttribute('style','text-align: left;');
+			div.append(document.createElement('br'));
 
-	     quote.quote.forEach((e,idx,arr) => {
-			if (idx == 0) {
-				if (arr.length == idx + 1) {
-					div.append(`"${e}"`)
-				} else {
-					div.append(`"${e}`)
+			// add an opening quote to the quote
+			str = '"'
+
+			quote.quote.forEach((e,idx,arr) => {
+				str += e
+				if (arr.length != idx + 1) {
+					if (formatted) {
+						// This is the case that the quote is really a multi-line entry instead of
+						// one long entry and needs to be displayed in its own line
+						str += "<br/>"
+					} else {
+						// Add a space at the end to avoid concatenating words together.
+						str += ' '
+					}
 				}
-			} else if (arr.length == idx + 1) {
-				div.append(`${e}"`)
-			} else {
-				div.append(e);
+			});
+
+			// add a closing quote to the quote
+			str += '"'
+
+			// Parse the generated string from the particular choosen quote
+			parsed = new DOMParser().parseFromString(str, "text/html")
+
+			// add all of the generated children nodes to the newly create div element
+			for (const entry of parsed.childNodes) {
+				div.append(entry)
 			}
-			if (formatted) {
-				// This is the case that the quote is really a poem that needs to be written
-				// on a different line.
-				div.append(document.createElement('br'));
-			} else {
-				// Add a space at the end to avoid concatinating words together.
-				div.append(' ');
-			}
-	     });
 
-	     elm.append(div);
+			elm.append(div);
 
-	     var div = document.createElement('div');
+			var div = document.createElement('div');
 
-	     div.setAttribute('style','text-align: right;');
-	     div.append(document.createElement('br'));
-	     div.append(quote.author);
-	     div.append(document.createElement('br'));
+			div.setAttribute('style','text-align: right;');
+			div.append(document.createElement('br'));
+			div.append(quote.author);
+			div.append(document.createElement('br'));
 
-	     elm.append(div);
+			elm.append(div);
 
-	     console.debug('GetQuote: applied the quote to the web page.');
+			console.debug('GetQuote: applied the quote to the web page.');
 
-	   };
-	   request.onerror = () => {
-	     console.error('GetQuote: An error was received when perform the quotes request.');
-	   };
-	   request.onabort = () => {
-	     console.error('GetQuote: The request was aborted');
-	   };
-	   request.ontimeout = () => {
-	     console.error('GetQuote: The request timed out');
-	   };
-	   request.onloadstart = () => {
-	     console.debug('GetQuote: the request called was started.');
-	   };
-	   request.onloadend = () => {
-	     console.debug('GetQuote: the request called has completed.');
-	   };
-	   console.debug('GetQuote: Exiting getQuote call');
+		}; // request.onload = () => {
+		request.onerror = () => {
+			console.error('GetQuote: An error was received when perform the quotes request.');
+		};
+		request.onabort = () => {
+			console.error('GetQuote: The request was aborted');
+		};
+		request.ontimeout = () => {
+			console.error('GetQuote: The request timed out');
+		};
+		request.onloadstart = () => {
+			console.debug('GetQuote: the request called was started.');
+		};
+		request.onloadend = () => {
+			console.debug('GetQuote: the request called has completed.');
+		};
+		console.debug('GetQuote: Exiting getQuote call');
 	};
