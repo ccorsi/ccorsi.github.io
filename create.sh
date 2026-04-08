771679c0ff02d251c72c5fe058afabc1507a4ea7
Claudio Corsi <clcorsi@yahoo.com>
2026-04-07 20:26:35 -0400

    Add more detailed help information about the different options that the create script offers.  This is more to be to not have to remember how to use the script.
    Also made a minor non-script like update to the commonrc loaded script.

--- create.sh
+++ create.sh
@@ -24,11 +24,271 @@
 ############################### Functions ##############################
 
 function usage {
-   echo "Usage: $0 -v -h -D"
+   case "$1" in
+      page)
+         usage_page
+         ;;
+      post)
+         usage_post
+         ;;
+      sidebar)
+         usage_sidebar
+         ;;
+      subbar)
+         usage_subbar
+         ;;
+      update)
+         usage_update
+         ;;
+   esac
+   
+   echo "Usage: $0 -v -h -D -H [page|sidebar|subbar|post|update]"
    echo "   -v: enable tracing of calls"
    echo "   -D: enable command echo"
    echo "   -h: print this message and exit"
+   echo "   -H [page|sidebar|subbar|post|update]: prints detailed help and exits"
    exit 1
+}
+
+# This text will provide a common overview of the different detailed help so
+# that the user can then understand the common reason for this type of help
+# within this script
+HEADER=$(cat << 'EOF'
+This script provides several different ways of creating and updating the current
+type of web pages and their menus.
+
+The objective is to provide a common means of producing web pages, including posts,
+for this web site such that the design is consistent throughout the site.
+
+The following describes the specific detailed help associated to the requested feature.
+
+EOF
+)
+
+function usage_page {
+   OUTPUT=$(cat << EOF
+$HEADER
+
+This selection is used to create web pages that are not posts.  Those type of pages
+will require that you select that post option of the list of options.
+
+The difference between creating a post and page is that the page will allow you to
+associate a embedded menu providing better movement and separations between web pages.
+
+The creation of a new web page will require that one has already created an associated
+sidebar that contains the different references associated with the creating web page.
+
+While the list of options will become clear as you are asked to input.  There is a
+few notes that should be mentioned.
+
+The permenent link of the generated web page should be started with a '/' character
+and should end with '/' for pages that will generate an index file.  While web pages
+that will not generate an index file that would require that you include the resulting
+generated file name.  For example, a indexed generated file for a permalink:
+
+   /topfolder/subfolder/
+
+note that you need to include the '/' as a prefix and suffix.
+
+As for a non-indexed file example for a permalink:
+
+   /topfolder/subfolder/filename.html
+
+again you need to prefix the name with the '/' character.
+
+The other note is that the generated filename for the file that will contain
+the text used to generate the web page.  You should include the required
+suffix so that Jekyll will be able to correctly generate the web page. For
+example:
+
+   topdirectory/subdirectory/filename.md
+
+The above will then allow Jekyll to correctly parse the input used to generate
+the web page.
+
+EOF
+)
+
+   echo "$OUTPUT" | less
+}
+
+function usage_sidebar {
+   OUTPUT=$(cat << EOF
+$HEADER
+
+This selection is used to create a sidebar to used by a page.  This sidebar is a in page
+menu that the user can use to navigate through the current page.  The reason behind creating
+a sidebar is that the current general navigation bar is limited to the number of levels deep
+that is can be defined.  The current navigation bar on the top of each page can only go one
+level deep.  Thus, the use of sidebars allow us the ability to produce links to multiple
+pages within a single topic/selection.
+
+The creation of a sidebar should be done after you've created all the referencing subbars
+associated with the sidebar.  Granted, this tool does provide the ability to append to a
+selected sidebar after it has been created.  It is better that you initially organize the
+way that you would like to navigate your sidebar.
+
+The creation of a sidebar should reference one or more of the following types of subbars.
+
+   - a "HOME" subbar
+   - a "INDIVIDUAL" subbar
+
+The above types of subbar provide logic that makes it easier to create and use the different
+subbars that are used by a sidebar.  These types of subbars are further explained within the
+subbar help sectio of this script.  We are then going to continue describing how a sidebar
+is created.
+
+A sidebar is created using the "HOME" subbar to reference a different page within the current
+topic/section.  This subbar is usually defined as "<section_name>[-<subsection_name>]*-home",
+where the <section_name> and <subsection_name>'s coorespond to the names of the levels of the
+sidebar topic/selection.
+
+While the use of the "INDIVIDUAL" subbar references are used to link the different topic specific
+pages that is related to the current topic/section.  This can be anything from in host pages or
+external links associated with the current topic/section.
+
+EOF
+)
+
+   echo "$OUTPUT" | less
+}
+
+function usage_subbar {
+   OUTPUT=$(cat << EOF
+$HEADER
+
+This selection is used to create a subbar. Subbars are associated with a sidebar and offer specific
+links for navigation within the current topic/section.  These navigations can be anything from
+going to a top-level page to pointing to a sub-topic/sub-section of the current topic/section.
+Thus, different subbars can be created depending on what they are used for.  While there is a
+flexible feature of creating and using a subbar.  There is a recommended design and usage of
+subbars.  These will be described in the next section.
+
+There are two different types of subbars that should be created.
+
+   - HOME
+   - INDIVIDUAL
+
+The above types will be able to seperate how each will be used within the context of a sidebar.
+The HOME subbar will contain a reference to a given page within the web site.  While an
+INDIVIDUAL subbar will contain references to links relevant to the current topic/section.  This
+will then be able to use the HOME subbar within a sidebar that references a different topic/section
+than the current page topic/section.  While the INDIVIDUAL subbar is used to reference all of the
+different topic/sections associated with the current topic/section.
+
+Let us describe the different options associated to creating a subbar.  A subbar can reference one
+or more entries and each entry can define one or more types.   Let us then expand on what each ones
+contain as options.
+
+The subbars list of entries can be one of the following:
+
+   - subbar
+   - entry
+   - submenu
+
+The subbar entry is just being able to reference another defined subbar that will be references within
+this currently defining subbar. 
+
+The entry options can then define the following options:
+
+   - url
+   - ext-url
+
+The url option is just a local reference to the site pages.  While the ext-url is a link that is located
+outside of the web site.  This will open the page within a new tab allowing you to be able to come back
+to the site current page.
+
+The url option will define a url path relative to the root url path of the current site.  For instance,
+if we have the following path:
+
+   - /home/foo/bar/
+
+The above will then open current site web page at http{s}://home/foo/bar/.  Note that in the example
+above that a '/' was added at the end.  This is required whenever you want to open the define index
+web file that is located within the /home/foo/bar directory of the web site.  If you want to reference
+a web page that is not the default.  You need to include the fully qualified web page like:
+
+   - /home/foo/bar/list.html
+
+This will then be referencing the specific local web page.
+
+The ext-url option will define an external link that will be opened within its own browser tab.  This
+will then allow you to be able to move back and fourth between the current web site and the external
+web site.
+
+Lastly, the submenu option just allows you to create a section that will contain similar topic/selection
+within the submenu.  Upon creating the submenu, you can then add one or more of the entry, subbar or
+submenu.
+
+EOF
+)
+
+   echo "$OUTPUT" | less
+}
+
+function usage_post { 
+   OUTPUT=$(cat << EOF
+$HEADER
+
+This selection is used to create a post page that is different from a regular web page using the
+page option.  There are differences between creating a post and page.  The main difference is that
+a post will request other types of information that will be attached to the creating post.  This
+information is specific to a regular web site post that is not associated with a regular web page.
+
+The creation of a post will then prompt you for different information associated with the newly
+creating post.  While some of them are obvious and these will not be mentioned here.  Thus, we will
+only discuss the ones that aren't as obvious options.
+
+You will be asked to associate a list of tags to your post.  These tags are used as a search mechanism
+for your posts.  It allows visitors the ability to find specific topics that they would be interested
+in reading.
+
+The next option are categories that you would enter for the creating post.  Categories are used to
+associate a general topic instead of specific identities like tags.  This allows the site to be
+organized to reflect the seperation of the post themselves.  While creating different categories is
+a great idea.  There is the caveat that you should keep the number of categories under ten for better
+site organizations.  While this option can be defined here.  It isn't limited to defining them here.
+One can create seperate categories by just using the directory level definitions.  Thus, you can
+create all tech topics within the tech directory of the posts directory which defines the "tech"
+category.
+
+The next option is to enable debugging of your post generation.  This is only useful when you are
+trying to understand why a particular page generation is not what you are looking for.  It can be
+useful but then it was added such that it might be useful in the future.
+
+The next option is weather this post should be published or if it is still being developed.  This
+simplifies the need to complete your post as soon as possible.  This then will allow you the space
+and time to properly develop the post without having to keep the copy locally.
+
+The final step offer you an oppurtunity to add some initial content to your post when it is being
+created.  While this section doesn't offer the flexibility of an editor.  It nonetheless provides
+some beginning to your post.
+
+The last step requires that you associate a filename to this post.  The name of the file should
+also include the .md prefix for it to be properly processed using Jekyll.  The name of the file
+post doesn't require it to be preceeded by '/' since these files will be created within the posts
+directory.
+
+EOF
+)
+
+   echo "$OUTPUT" | less
+}
+
+function usage_update {
+   OUTPUT=$(cat << EOF
+$HEADER
+
+This selection is used whenever you need to make changes to the currently created sidebar and subbar.
+The current implementation of this feature is that you can only add to the current sidebar and subbar.
+If you want to replace any of the entries, you would need to edit these implementations directly.
+Hopefully, in the future we can then update the current set of entries instead of only be able to add
+to the current entries for the selected sidebar and/or subbar.
+
+EOF
+)
+
+   echo "$OUTPUT" | less
 }
 
 ########################################################################
@@ -470,6 +730,7 @@ function create_post {
    get_line_input filename "Enter post file name without date prefix [YYYY-MM-DD-]" true
    echo
 
+   # TODO: we need to be able to determine if the filename contains category definitions
    # Determine if the _post directory exists
    if [ ! -d "${directory}/_post" ]
    then
@@ -666,6 +927,22 @@ debug=false
 get_script_name name $0
 get_script_dir directory $0
 
+# Process the command line arguments
+while getopts "H:hDv" o; do
+   case "${o}" in
+      v) debug=true
+         ;;
+      D) set -x
+         ;;
+      h) usage
+         ;;
+      H) usage $OPTARG
+         ;;
+      *) usage
+         ;;
+   esac
+done
+
 trace Calling $name located in $directory
 
 echo Gathering sidebars
@@ -683,16 +960,4 @@ echo Done gathering subbars
 # Add trap to insure that the temporary file is deleted if not used
 trap cleanup INT SIGINT
 
-# Process the command line arguments
-while getopts "Dvh" o; do
-   case "${o}" in
-      v) debug=true
-         ;;
-      D) set -x
-         ;;
-      h) usage
-   esac
-done
-
 create_selection
-
